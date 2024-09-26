73d43b34282b0f3143b143e85982d95fdd73a844
Avi-17 <workhorseavi17@gmail.com>
2024-09-26 22:23:13 +0530

    sm_commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { useState, useContext, useEffect } from "react";
 import "./App.css";
-import { getMonth } from "./util";
-import CalendarHeader from "./components/CalendarHeader";
-import Sidebar from "./components/Sidebar";
-import Month from "./components/Month";
-import GlobalContext from "./context/GlobalContext";
-import EventModal from "./components/EventModal";
+// import { getMonth } from "./util";
+import CalendarHeader from "./components/CalendarHeader.js";
+import Sidebar from "./components/Sidebar.js";
+
+import GlobalContext from "./context/GlobalContext.js";
+import EventModal from "./components/EventModal.js";
+import Day from "./components/Day.js";
+import Week from "./components/Week.js"; // Import the login page
+import Month from "./components/Month.js";
+import { getMonth, getWeek, getDay } from "./util.js";
+import dayjs from "dayjs";
+
+
 function App() {
-  const [currenMonth, setCurrentMonth] = useState(getMonth());
-  const { monthIndex, showEventModal } = useContext(GlobalContext);
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex, daySelected, showEventModal } = useContext(GlobalContext);
+  const [viewState, setViewState] = useState("Month");
 
   useEffect(() => {
-    setCurrentMonth(getMonth(monthIndex));
-  }, [monthIndex]);
+    if (viewState === "Month") {
+      setCurrentMonth(getMonth(monthIndex));
+    }
+  }, [monthIndex, viewState]);
+  
+  const currentDay = daySelected || getDay();
+  const daysInWeek = getWeek(null, daySelected ? daySelected.month() : dayjs().month());
+
+  
+
 
   return (
     <React.Fragment>
       {showEventModal && <EventModal />}
 
       <div className="h-screen flex flex-col">
-        <CalendarHeader />
+        <CalendarHeader setViewState={setViewState} />
         <div className="flex flex-1">
           <Sidebar />
-          <Month month={currenMonth} />
+          {viewState === "Month" && <Month month={currentMonth} />}
+          {viewState === "Week" && <Week daysInWeek={daysInWeek} />}
+          {viewState === "Day" && <Day day={currentDay} rowIdx={0} />}
         </div>
       </div>
     </React.Fragment>
   );
 }
+
 
 export default App;
